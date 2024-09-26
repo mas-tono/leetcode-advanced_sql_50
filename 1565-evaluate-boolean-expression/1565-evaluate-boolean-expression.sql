7ec8780146d40d556acf364b15951c47246e66fa
hartono <124220894+mas-tono@users.noreply.github.com>
2024-09-26 18:29:55 +0700

    Time: 2028 ms (55.42%), Space: 0B (100%) - LeetHub

--- 1565-evaluate-boolean-expression/1565-evaluate-boolean-expression.sql
+++ 1565-evaluate-boolean-expression/1565-evaluate-boolean-expression.sql
@@ -1,5 +1,15 @@
 /* Write your T-SQL query statement below */
 
+-- lesson learned:
+-- we can't just come and doing comparison operator = '>' then left_operand > right_operand and so on becaue sql doesn't allow it
+
+-- here’s why it doesn't work:
+
+--     SQL evaluates operator = ">" as a string comparison, not an actual operation.
+--     SQL does not dynamically execute conditions like left_operand > right_operand based on the value of the operator string.
+
+-- we need to break the problem down by using individual CASE expressions for each comparison instead of trying to perform the comparison dynamically.
+
 with firstt as (
                 select e.left_operand, 
                         e.operator, 
