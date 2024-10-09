e44e5ae344b767f6db689843c585c6d4a0d615cd
hartono <124220894+mas-tono@users.noreply.github.com>
2024-10-09 15:37:38 +0700

    Time: 1190 ms (38.2%), Space: 0B (100%) - LeetHub

---
+++ 1842-number-of-calls-between-two-persons/1842-number-of-calls-between-two-persons.sql
@@ -0,0 +1,18 @@
+/* Write your T-SQL query statement below */
+
+with firstt as (select *,
+    case
+        when from_id < to_id then from_id else to_id
+    end as person1,
+    case
+        when to_id > from_id then to_id else from_id
+    end as person2
+from Calls),
+
+
+secondd as (select person1, person2, concat(person1, person2) as joinn, duration
+from firstt)
+
+select person1, person2, count(joinn) as call_count, sum(duration) as total_duration
+from secondd
+group by person1, person2
